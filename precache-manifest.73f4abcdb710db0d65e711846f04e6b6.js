7787d5013eecb102f8ebab58ac9f6274892c204b
Gary Aloisio <garyjaloisio@gmail.com>
2021-05-04 13:12:50 -0400

    Updates

---
+++ precache-manifest.73f4abcdb710db0d65e711846f04e6b6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e1fedabf47abae28e651",
+    "url": "/static/js/main.e1fedabf.chunk.js"
+  },
+  {
+    "revision": "329e518657586fa83ba9",
+    "url": "/static/js/1.329e5186.chunk.js"
+  },
+  {
+    "revision": "e1fedabf47abae28e651",
+    "url": "/static/css/main.6285a26f.chunk.css"
+  },
+  {
+    "revision": "9f4ba23985f9aba9d21d21e42cb3ea07",
+    "url": "/index.html"
+  }
+];
